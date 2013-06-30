e6123b4ef59e4db0dfe81c5ba5cd001ee8774456
Matthew Saeed <saeedm@amazon.com>
2013-06-30 00:22:19 -0700

    pg

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Myapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-root to: 'index#index'
+  #root to: 'index#index'
+root "welcome#index"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
