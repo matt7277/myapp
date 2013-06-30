9568f0f436fb211728a25176d63550a05453d8c9
Matthew Saeed <saeedm@amazon.com>
2013-06-30 00:05:22 -0700

    pg

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 #gem  'rails', '3.2.13'
 
+gem 'sinatra', '1.1.0'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Myapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+root to: 'index#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
